8a9241c588a83c1c20bfccbb1a6bb9ac582a98e6
user <user@this.com>
2019-11-06 13:27:40 -0500

    Added base SQL structure

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Users (Name, Password, Email) VALUES ('Name1', 'Password1', 'Email1');
+INSERT INTO Users (Name, Password, Email) VALUES ('Name2', 'Password2', 'Email2');
+INSERT INTO Users (Name, Password, Email) VALUES ('Name3', 'Password3', 'Email3');
